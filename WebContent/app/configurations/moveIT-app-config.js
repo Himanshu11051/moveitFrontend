cc2668bb8eb7aa57b50c98688d8ab18a0352fde5
himanshu11051 <himanshu11051@iiitd.ac.in>
2016-09-03 23:39:43 +0530

    Integrated with angular

--- WebContent/app/configurations/moveIT-app-config.js
+++ WebContent/app/configurations/moveIT-app-config.js
@@ -1,3 +1,21 @@
 /* Move It App Config File
  * @Author: Himanshu
  */ 
+var moveitApp = {
+		bootstrap : function(){
+			angular.module('moveitApp',['ui.router', 'ngResource', 'ngMaterial', 'ngAnimate', 'ngMessages']);
+		},
+		routerConfig : function(){
+			angular.module('moveitApp').config(['$urlRouterProvider', '$stateProvider', function (urlRouterProvider, stateProvider) {
+
+				urlRouterProvider.otherwise('home');
+				stateProvider.state('home', {
+					url: '/home',
+					templateUrl: 'app/views/home.html',
+					controller: 'moveitAppCtrl'
+				})
+			}]);
+		}
+}
+moveitApp.bootstrap();
+moveitApp.routerConfig();
--- WebContent/app/controllers/moveIT-app-controller.js
+++ WebContent/app/controllers/moveIT-app-controller.js
@@ -1,3 +1,16 @@
 /* Move It App Controller File
  * @Author: Himanshu
  */ 
+var moveitAppActions = {
+	moveitAppCtrl : function(){
+		angular.module('moveitApp').controller('moveitAppCtrl',['$rootScope','$scope','$mdToast',function($rootscope, $scope, $mdToast){
+			/*$scope.showToast = function (content) {
+				$mdToast.show($mdToast.simple().textContent(content).hideDelay(false));
+			};
+			
+				$scope.showToast('Welcome To Move It')*/
+		
+		}]);
+	},
+};
+moveitAppActions.moveitAppCtrl();
